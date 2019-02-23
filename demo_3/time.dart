46e702642ea8f2c5dfe5e4d0b3d80fa85389b9b8
MikuBlog <786601672@qq.com>
2019-02-23 19:19:50 +0800

    2019-02-23

---
+++ demo_3/time.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DateTime extends StatefulWidget {
+  @override
+  _DateTimeState createState() => _DateTimeState();
+}
+
+class _DateTimeState extends State<DateTime> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("日历"),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.timer),
+        onPressed: () {
+          // 调用函数打开
+          showTimePicker(
+            context: context,
+            initialTime: new TimeOfDay.now(),
+          ).then((val) {
+            print(val);
+          }).catchError((err) {
+            print(err);
+          });
+        },
+      ),
+    );
+  }
+
+}
